9fcbfc3f068a80a3af841e27585704a741200a25
NaveedMohammed <rmdnaveed@gmail.com>
2021-03-23 12:41:10 -0500

    Adding first extension template

---
+++ src/first-extension.js
@@ -0,0 +1,35 @@
+function FirstExtension() {
+    
+    firstExtension = function() {};
+    firstExtension.prototype = new Extension('FirstExtension');
+    firstExtension.prototype.getMenu = () => {
+        return {
+            'Will do something, someday': function() {
+            },
+        };
+    };
+    firstExtension.prototype.getCategories = () => [
+        new Extension.Category(
+            'first-extension',
+            new Color(10, 100, 10),
+            [
+                '-',
+                '-',
+                '-',
+                'setCustomValue'
+            ]
+        )
+    ];
+    firstExtension.prototype.getBlocks = () => [
+        new Extension.Block(
+            'setCustomValue',
+            'command',
+            'first-extension',
+            'set customValue to %n',
+            [0],
+            () => 'This is a test.'
+        )
+    ];
+
+    NetsBloxExtensions.register(firstExtension);
+}
